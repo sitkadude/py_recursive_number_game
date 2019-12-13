1af24de596b29fcb9f47318a18695dc8279e1237
Lukas Jorgensen <lukas.r.jorgensen@gmail.com>
2019-12-13 15:11:17 -0500

    code edit

--- py_guess_random_number.py
+++ py_guess_random_number.py
@@ -1,3 +1,10 @@
-random_number = randint(1,6)
-guess_number = int(input("Guess a random number between 1 and 6! "))
+import random
+random_number = random.randint(1,6)
+user_name = input("What is your name? ")
+guess_number = int(input("Welcome, {}! Please guess a random number between 1 and 6. ".format(user_name.capitalize())))
+
+if guess_number == random_number:
+    print("You got it! The correct number was {}! Thanks for playing!".format(random_number))
+else:
+    print("WRONG! The correct answer was {}. Thanks for playing!".format(random_number))
 
