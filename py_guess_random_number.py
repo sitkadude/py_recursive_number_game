e6dc68aa87c846e792cfa96e4ae1da58930528fc
Lukas Jorgensen <lukas.r.jorgensen@gmail.com>
2019-12-13 17:22:45 -0500

    added except valueError

--- py_guess_random_number.py
+++ py_guess_random_number.py
@@ -1,8 +1,14 @@
 import random
 random_number = random.randint(1,6)
-user_name = input("What is your name? ")
-guess_number = int(input("Welcome, {}! Please guess a random number between 1 and 6. ".format(user_name.capitalize())))
+user_name = input("What is your name?: ")
 
+
+while True:
+    try:
+        guess_number = int(input("Welcome, {}! Please guess a random number between 1 and 6. ".format(user_name.capitalize())))
+        break
+    except ValueError:
+        print("Oops! That was not an integer. Please try again:")
 
 if guess_number == random_number:
     print("You got it! The correct number was {}! Thanks for playing!".format(random_number))
