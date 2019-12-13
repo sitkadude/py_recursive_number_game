985f1f814ff420f87a79335c19baae348438a7e0
Lukas Jorgensen <lukas.r.jorgensen@gmail.com>
2019-12-13 16:34:20 -0500

    code edit

--- py_guess_random_number.py
+++ py_guess_random_number.py
@@ -3,8 +3,15 @@ random_number = random.randint(1,6)
 user_name = input("What is your name? ")
 guess_number = int(input("Welcome, {}! Please guess a random number between 1 and 6. ".format(user_name.capitalize())))
 
+
 if guess_number == random_number:
     print("You got it! The correct number was {}! Thanks for playing!".format(random_number))
+
+elif guess_number > random_number:
+    second_guess = input("The number is higher is lower than {}. Please guess again: ".format(guess_number))
+
+elif guess_number < random_number:
+    second_guess = input("The number is smaller is lower than {}. Please guess again: ".format(guess_number))
 else:
     print("WRONG! The correct answer was {}. Thanks for playing!".format(random_number))
 
